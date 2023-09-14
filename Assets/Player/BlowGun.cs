0cc59c8f66233a96a9b5a344789c779bb15ceef7
전해성 <seng19292@gmail.con>
2023-09-14 21:03:21 +0900

    플레이어 블로우건 fix

--- Assets/Player/BlowGun.cs
+++ Assets/Player/BlowGun.cs
@@ -8,6 +8,8 @@ public class BlowGun : MonoBehaviour
 
     public bool triggerShot = false;
     public bool holdHand = false;
+    public bool hand;
+    public GameObject air;
 
 
     //private void OnTriggerEnter(Collider other)
@@ -44,27 +46,41 @@ public class BlowGun : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Hand"))
         {
+            hand = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(hand == true)
+        {
             if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) >= 0.8f)
             {
+                holdHand = true;
                 if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) >= 0.8f)
                 {
                     airForce.Play();
+                    air.gameObject.SetActive(true);
                 }
                 else
                 {
                     airForce.Pause();
+                    air.gameObject.SetActive(false);
                 }
             }
 
             if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) >= 0.8f)
             {
+                holdHand = true;
                 if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) >= 0.8f)
                 {
                     airForce.Play();
+                    air.gameObject.SetActive(true);
                 }
                 else
                 {
                     airForce.Pause();
+                    air.gameObject.SetActive(false);
                 }
             }
         }
